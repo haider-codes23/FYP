3f9058221923528a8bfba58139e886fff51e105e
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-24 09:04:06 +0500

    Implemented Perks compoenent within PlacePage.jsx

--- client/src/Perks.jsx
+++ client/src/Perks.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import parkingArea from './assets/parking-area.png';
 import airConditioner from './assets/air-conditioner.png';
 import privateEntrance from './assets/leave.png';
@@ -11,60 +12,71 @@ import roomCleaningService from './assets/sweeping.png';
 import wifi from './assets/wifi.png';
 
 export default function Perks({selected, onChange}) {
+  function handleCheckboxClick(event) {
+    const {checked, name} = event.target;
+    if (checked) {
+      onChange([...selected, name]);
+    } else {
+      onChange([...selected.filter(selectedName => selectedName !== name)]);
+    }
+
+    // onChange([...selected], name);
+  }
+
   return (
     <>
       <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox"  />
+                <input type="checkbox" name="wifi" onChange={handleCheckboxClick} />
                 <img src={wifi} className='w-10 h-10' />
                 <span>Wifi</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='airConditioner' onChange={handleCheckboxClick} />
                 <img src={airConditioner} className='w-10 h-10 mt-4' />
                 <span>Air conditioner</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='parking' onChange={handleCheckboxClick} />
                 <img src={parkingArea} className='w-12 h-12 mb-2' />
                 <span>Free parking premises</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='privateEntrance' onChange={handleCheckboxClick} />
                 <img src={privateEntrance} className='w-10 h-10' />
                 <span>Private Entrance</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='washer' onChange={handleCheckboxClick} />
                 <img src={washer} className='w-10 h-10' />
                 <span>Washer</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='kitchen' onChange={handleCheckboxClick} />
                 <img src={kitchen} className='w-10 h-10 mb-2' />
                 <span>Kitchen</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='bathroomAmenities' onChange={handleCheckboxClick} />
                 <img src={bathroomAmenities} className='w-10 h-10' />
                 <span>Bathroom amenites </span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='tv' onChange={handleCheckboxClick} />
                 <img src={tv} className='w-10 h-10' />
                 <span>TV</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='pets' onChange={handleCheckboxClick} />
                 <img src={pets} className='w-10 h-10' />
                 <span>Pets Allowed</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='privateSecurity' onChange={handleCheckboxClick} />
                 <img src={privateSecurity} className='w-10 h-10' />
                 <span>Private Secuity</span>
               </label>
               <label className="border hover:border-indigo-500/75 p-4 gap-2 flex rounded-2xl items-center cursor-pointer">
-                <input type="checkbox" />
+                <input type="checkbox" name='roomCleaningService' onChange={handleCheckboxClick} />
                 <img src={roomCleaningService} className='w-12 h-12' />
                 <span>Room Cleaning service</span>
               </label>
